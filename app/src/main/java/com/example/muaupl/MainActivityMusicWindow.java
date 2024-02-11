8087aa377e0b6b5fad7543c044ecd39ca543a720
nikit <nglaz12@yandex.ru>
2024-02-12 02:40:58 +0700

    add showDialog

---
+++ app/src/main/java/com/example/muaupl/MainActivityMusicWindow.java
@@ -0,0 +1,81 @@
+package com.example.muaupl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.Menu;
+import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivityMusicWindow extends AppCompatActivity {
+    private Toolbar mToolbar;
+    public static final int SORT_BY_NAME_ID = R.id.sort_by_name;
+    public static final int SORT_BY_DATE_ID = R.id.sort_by_date;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_music_window);
+
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("");
+        mToolbar.setLogo(R.drawable.icomusic);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+
+        MenuItem item = menu.findItem(R.id.action_search);
+        MenuItem searchItem = menu.findItem(R.id.action_search_input);
+        EditText editTextSearch = searchItem.getActionView().findViewById(R.id.edit_text_search);
+
+        //изменение цвета кнопки для поиска
+        Drawable searchIcon = item.getIcon();
+        if (searchIcon != null) {
+            searchIcon.mutate();
+            searchIcon.setColorFilter(ContextCompat.getColor(this, R.color.white), PorterDuff.Mode.SRC_IN);
+            item.setIcon(searchIcon);
+        }
+
+
+        searchItem.setOnActionExpandListener(new MenuItem.OnActionExpandListener() {
+            @Override
+            public boolean onMenuItemActionExpand(MenuItem item) {
+                // Расширение поля для ввода текста
+                return true;
+            }
+
+            @Override
+            public boolean onMenuItemActionCollapse(MenuItem item) {
+                // Сворачивание поля для ввода текста
+                return true;
+            }
+        });
+
+        editTextSearch.setOnEditorActionListener((v, actionId, event) -> {
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                // Действия при нажатии на кнопку "Готово" на клавиатуре
+                String searchText = editTextSearch.getText().toString();
+                performSearch(searchText);
+                return true;
+            }
+            return false;
+        });
+        return true;
+    }
+    private void performSearch(String query) {
+        // Обработка поискового запроса
+        // Здесь вы можете добавить логику для поиска и отображения результатов
+        Toast.makeText(this, "Поиск: " + query, Toast.LENGTH_SHORT).show();
+    }
+}
--- app/src/main/java/com/example/muaupl/SplashScreen.java
+++ app/src/main/java/com/example/muaupl/SplashScreen.java
@@ -1,6 +1,5 @@
 package com.example.muaupl;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
@@ -8,118 +7,48 @@ import android.Manifest;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 public class SplashScreen extends AppCompatActivity {
 
-    public static final int Request_Code_For_Permission = 1;
-    public static final int CAMERA_PERMISSION_CODE = 2;
+    private static final int PERMISSION_REQUEST_CODE = 1;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash_screen);
-        // Проверяем разрешение на чтение файлов
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            // Если разрешение не предоставлено, запрашиваем его
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, Request_Code_For_Permission);
-        } else {
-            // Разрешение уже предоставлено или запрашивалось ранее, переходим к следующей активности
-            goToNextActivity();
-        }
+        setContentView(R.layout.activity_main_music_window);
+
+        requestMediaFilePermission();
     }
 
-    private void checkPermissionAndProceed() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            // Если разрешение не предоставлено, или было отклонено ранее, показываем диалог запрашивающий разрешение
-            showPermissionDialog();
+    private void requestMediaFilePermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_MEDIA_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_MEDIA_AUDIO}, PERMISSION_REQUEST_CODE);
         } else {
-            // Разрешение уже предоставлено, переходим к следующей активности
-            goToNextActivity();
-        }
-    }
-
-    private void showPermissionDialog() {
-        // Проверяем, показывали ли уже окно запроса разрешения
-        if (!shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE)) {
-            // Show the rational explanation if the user denied the permission previously
-            AlertDialog.Builder rationaleBuilder = new AlertDialog.Builder(this);
-            rationaleBuilder.setMessage("Для продолжения необходимо предоставить разрешение на чтение файлов. Предоставить разрешение?");
-            rationaleBuilder.setPositiveButton("Да", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    // Пользователь нажал "Да", запрашиваем разрешение
-                    ActivityCompat.requestPermissions(SplashScreen.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, Request_Code_For_Permission);
-                }
-            });
-            rationaleBuilder.setNegativeButton("Нет", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    // Пользователь нажал "Нет", закрываем приложение
-                    finish();
-                }
-            });
-            rationaleBuilder.setCancelable(false);
-            AlertDialog rationaleDialog = rationaleBuilder.create();
-            rationaleDialog.show();
-        } else {
-            ActivityCompat.requestPermissions(SplashScreen.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, Request_Code_For_Permission);
+            // Если разрешение уже предоставлено, перейти к следующей активности
+            Intent intent = new Intent(SplashScreen.this, MainActivityMusicWindow.class);
+            startActivity(intent);
+            finish(); // Закрыть текущую активность
         }
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == Request_Code_For_Permission) {
+
+        if (requestCode == PERMISSION_REQUEST_CODE) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Разрешение на чтение файлов предоставлено пользователем, переходим к следующей активности
-                goToNextActivity();
+                // Разрешение получено, можно продолжить выполнение операций
+                Intent intent = new Intent(SplashScreen.this, MainActivityMusicWindow.class);
+                startActivity(intent);
+                finish(); // Закрыть текущую активность
             } else {
-                // Разрешение на чтение файлов не предоставлено, показываем диалоговое окно с сообщением об отказе
-                showPermissionDialog();
-            }
-        } else if (requestCode == CAMERA_PERMISSION_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Разрешение на камеру предоставлено пользователем, переходим к следующей активности
-                goToNextActivity();
-            } else {
-                // Разрешение на камеру не предоставлено, показываем диалоговое окно с сообщением об отказе
-                showCameraPermissionDeniedDialog();
+                // Разрешение было отклонено, показать пользователю уведомление об этом
+                Toast.makeText(this, "Разрешение на чтение медиафайлов не было предоставлено", Toast.LENGTH_SHORT).show();
+                finish(); // Закрыть текущую активность
             }
         }
     }
-
-    private void goToNextActivity() {
-        // Здесь можно запустить следующую активность
-        Intent intent = new Intent(SplashScreen.this, MainActivity.class);
-        startActivity(intent);
-        finish();
-    }
-
-    private void showCameraPermissionDeniedDialog() {
-        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
-        dialogBuilder.setMessage("Для использования камеры приложению необходимо разрешение на доступ к камере. Хотите предоставить разрешение?");
-        dialogBuilder.setPositiveButton("Да", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                // Пользователь нажал "Да", запрашиваем разрешение на камеру
-                ActivityCompat.requestPermissions(SplashScreen.this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
-            }
-        });
-        dialogBuilder.setNegativeButton("Нет", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                // Пользователь нажал "Нет", закрываем приложение или делаем необходимые действия
-                finish();
-            }
-        });
-        dialogBuilder.setCancelable(false);
-        AlertDialog permissionDialog = dialogBuilder.create();
-        permissionDialog.show();
-    }
-
 }

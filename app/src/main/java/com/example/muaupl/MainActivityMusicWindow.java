e14822049d1826ca06a7bab109dd85de2d4373c5
nikit <nglaz12@yandex.ru>
2024-02-14 19:05:35 +0700

    add new function

--- app/src/main/java/com/example/muaupl/MainActivityMusicWindow.java
+++ app/src/main/java/com/example/muaupl/MainActivityMusicWindow.java
@@ -2,22 +2,33 @@ package com.example.muaupl;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivityMusicWindow extends AppCompatActivity {
     private Toolbar mToolbar;
+    private boolean isSearchBarOpened = false;
+    MenuItem searchItem;
+    MenuItem item;
+    EditText editTextSearch;
     public static final int SORT_BY_NAME_ID = R.id.sort_by_name;
     public static final int SORT_BY_DATE_ID = R.id.sort_by_date;
     @Override
@@ -35,11 +46,58 @@ public class MainActivityMusicWindow extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.toolbar_menu, menu);
 
-        MenuItem item = menu.findItem(R.id.action_search);
-        MenuItem searchItem = menu.findItem(R.id.action_search_input);
-        EditText editTextSearch = searchItem.getActionView().findViewById(R.id.edit_text_search);
+        searchItem = menu.findItem(R.id.action_search_input);
+        item = menu.findItem(R.id.action_search);
+        editTextSearch = searchItem.getActionView().findViewById(R.id.edit_text_search);
+        editTextSearch.setVisibility(View.INVISIBLE);
 
-        //изменение цвета кнопки для поиска
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        ViewPager viewPager = findViewById(R.id.viewPager);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragment(new TracksFragment(), "Музыка");
+        viewPagerAdapter.addFragment(new PlaylistsFragment(), "Плейлисты");
+
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+
+
+        item.setOnMenuItemClickListener(menuItem -> {
+            if (!isSearchBarOpened) {
+                isSearchBarOpened = true;
+                editTextSearch.requestFocus();
+                item.setIcon(R.drawable.iconsclose);
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.showSoftInput(editTextSearch, InputMethodManager.SHOW_IMPLICIT);
+            } else {
+                isSearchBarOpened = false;
+                editTextSearch.setText("");
+                item.setIcon(R.drawable.iconsearch);
+                searchItem.collapseActionView();
+                editTextSearch.clearFocus();
+            }
+            return true;
+        });
+
+        editTextSearch.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (s.length() > 0) {
+                    item.setIcon(R.drawable.iconsclose);
+                } else {
+                    item.setIcon(R.drawable.iconsearch);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
         Drawable searchIcon = item.getIcon();
         if (searchIcon != null) {
             searchIcon.mutate();
@@ -47,35 +105,57 @@ public class MainActivityMusicWindow extends AppCompatActivity {
             item.setIcon(searchIcon);
         }
 
-
-        searchItem.setOnActionExpandListener(new MenuItem.OnActionExpandListener() {
-            @Override
-            public boolean onMenuItemActionExpand(MenuItem item) {
-                // Расширение поля для ввода текста
-                return true;
-            }
-
-            @Override
-            public boolean onMenuItemActionCollapse(MenuItem item) {
-                // Сворачивание поля для ввода текста
-                return true;
-            }
-        });
-
         editTextSearch.setOnEditorActionListener((v, actionId, event) -> {
             if (actionId == EditorInfo.IME_ACTION_DONE) {
-                // Действия при нажатии на кнопку "Готово" на клавиатуре
                 String searchText = editTextSearch.getText().toString();
                 performSearch(searchText);
+                editTextSearch.setVisibility(View.INVISIBLE);
+                editTextSearch.setText("");
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(editTextSearch.getWindowToken(), 0);
                 return true;
             }
             return false;
         });
+
         return true;
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_search) {
+            if (!isSearchBarOpened) {
+                isSearchBarOpened = true;
+                editTextSearch.setVisibility(View.VISIBLE);
+                editTextSearch.requestFocus();
+
+                item.setIcon(R.drawable.iconsclose);
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.showSoftInput(editTextSearch, InputMethodManager.SHOW_IMPLICIT);
+            } else {
+                isSearchBarOpened = false;
+                editTextSearch.setVisibility(View.INVISIBLE);
+                editTextSearch.setText("");
+
+                item.setIcon(R.drawable.iconsearch);
+                InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(editTextSearch.getWindowToken(), 0);
+            }
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
     private void performSearch(String query) {
-        // Обработка поискового запроса
-        // Здесь вы можете добавить логику для поиска и отображения результатов
-        Toast.makeText(this, "Поиск: " + query, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Search: " + query, Toast.LENGTH_SHORT).show();
+
+        isSearchBarOpened = false;
+        editTextSearch.setVisibility(View.VISIBLE);
+        editTextSearch.requestFocus();
+        item.setIcon(R.drawable.iconsclose);
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.showSoftInput(editTextSearch, InputMethodManager.SHOW_IMPLICIT);
+        editTextSearch.requestFocus();
     }
 }
--- app/src/main/java/com/example/muaupl/ViewPagerAdapter.java
+++ app/src/main/java/com/example/muaupl/ViewPagerAdapter.java
@@ -1,5 +1,6 @@
 package com.example.muaupl;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -12,17 +13,13 @@ import java.util.List;
 public class ViewPagerAdapter extends FragmentPagerAdapter {
 
     private final List<Fragment> fragmentList = new ArrayList<>();
-    private final List<String> fragmentTitleList = new ArrayList<>();
+    private final List<String> fragmentTitleList = new ArrayList();
 
-    public ViewPagerAdapter(FragmentManager fm) {
+    public ViewPagerAdapter(@NonNull FragmentManager fm) {
         super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
     }
 
-    public void addFragment(Fragment fragment, String title) {
-        fragmentList.add(fragment);
-        fragmentTitleList.add(title);
-    }
-
+    @NonNull
     @Override
     public Fragment getItem(int position) {
         return fragmentList.get(position);
@@ -31,6 +28,11 @@ public class ViewPagerAdapter extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         return fragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment, String title) {
+        fragmentList.add(fragment);
+        fragmentTitleList.add(title);
     }
 
     @Nullable
